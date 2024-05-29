f66b11de27966ee0fe43ee77c795231fd88d6773
Marcello C Bustamante <marcellusvii@hotmail.com>
2024-05-29 13:39:54 +0200

    first commit

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 from weather import main as get_weather
 
-app= Flask(__name__)
+app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     data = None
